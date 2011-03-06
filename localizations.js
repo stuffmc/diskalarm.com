74e0bc06cfb17b244c6e0197ad406fc33c246a76
StuFF mc <mc@stuffmc.com>
2011-03-06 18:23:12 +0100

    l10n.js now working with french and json!

---
+++ localizations.js
@@ -0,0 +1,16 @@
+String.toLocaleString({
+	"en": {
+		"%title": "English - l10n.js demo",
+		"%info": "You are viewing an English localization of this page."
+	},
+	"es": "es.json",
+	"fr": "fr.json",
+	"nl": {
+		"%title": "Nederlands - Demo van l10n.js",
+		"%info": "U ziet nu een Nederlandse lokalisatie van deze pagina."
+	},
+	"de": {
+		"%title": "Deutsch - l10n.js Demo",
+		"%info": "Sie sehen hier die deutsche Lokalisierung dieser Seite."
+	},
+});
