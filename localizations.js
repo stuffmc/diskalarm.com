51b5ef2f3485c5e58a755996eca6cf7f61409753
StuFF mc <mc@stuffmc.com>
2011-03-25 17:28:37 +0100

    added webfonts and added auto translate capabilities (which I should contribute to l10n.js)

--- localizations.js
+++ localizations.js
@@ -1,18 +1,32 @@
 String.toLocaleString({
 	"en": {
 		"%title": "Disk Alarm: Tell me before it's too late!",
+		"en": "",
+		"fr": "français",
+		"de": "deutsch",
+		"es": "español",
+		"nl": "nederlands",
 		"%info": "You are viewing an English localization of this page."
 	},
 	"es": "es.json",
 	"fr": "fr.json",
 	"nl": {
 		"%title": "Nederlands - Demo van l10n.js",
+		"en": "english",
+		"fr": "français",
+		"de": "deutsch",
+		"es": "español",
+		"nl": "",
 		"%info": "U ziet nu een Nederlandse lokalisatie van deze pagina."
 	},
 	"de": {
 		"%title": "111--Disk Alarm: Sag bescheid, wenn es zu spät ist!",
 		"%tellme": "Sag bescheid, wenn es zu spät ist!",
-		"%detext": "---",
+		"en": "english",
+		"fr": "français",
+		"de": "",
+		"es": "español",
+		"nl": "nederlands",
 		"%info": "Sie sehen hier die deutsche Lokalisierung dieser Seite."
 	},
 });
